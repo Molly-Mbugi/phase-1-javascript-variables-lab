b2adcb364a8d67f7b83b0b6e5f88560d27c4047e
molly <mollynzembi@gmail.com>
2024-03-19 13:44:03 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution in this file!
+// Code your solution in this file!/
+let companyName = 'Scuber';
+
+
+const companyNameConst = 'Scuber';
+
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = 'Susan Smith';
+
